2c1b4ec31ae54095627d6543bf05905b2d22b061
elyutikov <ealyutikov@gmail.com>
2022-05-16 21:53:02 +0300

    add lint

--- blockchain/block.go
+++ blockchain/block.go
@@ -1,5 +1,11 @@
 package blockchain
 
+import (
+	"bytes"
+	"encoding/gob"
+	"log"
+)
+
 type Block struct {
 	PreviosHash []byte
 	Hash        []byte
@@ -26,4 +32,30 @@ func CreateBlock(data string, prevHash []byte) *Block {
 
 func Genesis() *Block {
 	return CreateBlock("Genesis", []byte{})
+}
+
+func (b *Block) Serialize() []byte {
+	var res bytes.Buffer
+	encoder := gob.NewEncoder(&res)
+
+	err := encoder.Encode(b)
+	Handle(err)
+
+	return res.Bytes()
+}
+
+func Deserialize(data []byte) *Block {
+	var block Block
+	decoder := gob.NewDecoder(bytes.NewReader(data))
+
+	err := decoder.Decode(&block)
+	Handle(err)
+
+	return &block
+}
+
+func Handle(err error) {
+	if err != nil {
+		log.Panic(err)
+	}
 }
