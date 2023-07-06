6fbcb9220a1d2e35a1bfacc48f870facd0435279
ealyutikov <ealyutikov@gmail.com>
2023-07-06 13:05:50 +0300

    product-of-array-except-self

--- algorithms/product_of_array_except_self/product_of_array_except_self.go
+++ algorithms/product_of_array_except_self/product_of_array_except_self.go
@@ -1,5 +1,35 @@
 package product_of_array_except_self
 
+// https://leetcode.com/problems/product-of-array-except-self
 func productExceptSelf(nums []int) []int {
-	return nil
+	product := 1
+	left := make([]int, len(nums), len(nums))
+	for i := 0; i < len(nums); i++ {
+		left[i] = product * nums[i]
+		product *= nums[i]
+	}
+
+	product = 1
+	rigth := make([]int, len(nums), len(nums))
+	for i := len(nums) - 1; i >= 0; i-- {
+		rigth[i] = product * nums[i]
+		product *= nums[i]
+	}
+
+	result := make([]int, len(nums), len(nums))
+	for i := 0; i < len(nums); i++ {
+		var l, r int = 1, 1
+
+		if i-1 >= 0 {
+			l = left[i-1]
+		}
+
+		if i+1 < len(nums) {
+			r = rigth[i+1]
+		}
+
+		result[i] = l * r
+	}
+
+	return result
 }
--- algorithms/product_of_array_except_self/product_of_array_except_self_test.go
+++ algorithms/product_of_array_except_self/product_of_array_except_self_test.go
@@ -1,7 +1,23 @@
 package product_of_array_except_self
 
-import "testing"
+import (
+	"testing"
 
-func Test_productExceptSelf(t *testing.T) {
-	print("test")
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_productExceptSelf_1(t *testing.T) {
+	assert.Equal(
+		t,
+		[]int{24, 12, 8, 6},
+		productExceptSelf([]int{1, 2, 3, 4}),
+	)
+}
+
+func Test_productExceptSelf_2(t *testing.T) {
+	assert.Equal(
+		t,
+		[]int{0, 0, 9, 0, 0},
+		productExceptSelf([]int{-1, 1, 0, -3, 3}),
+	)
 }
